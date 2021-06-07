8b3210eddc6a1a5185c28dcb3eec3501e44bebf1
Varditli <vardit161@gmail.com>
2021-06-07 17:56:21 +0300

    back updates

--- app.js
+++ app.js
@@ -1,40 +1,43 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const PORT=5000
-const {MONGOURI}= require('./key')
-const cors = require('cors')
-app.use(cors())
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const PORT = 5000;
+const { MONGOURI } = require("./key");
+const cors = require("cors");
+app.use(cors());
 //btrPlpqOKmAIyqeG
 
 console.log(Date.now());
 
-mongoose.connect(MONGOURI,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+mongoose.connect(MONGOURI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
 
-mongoose.connection.on('connected',()=>{
-    console.log("connected to mongo yeah")
-})
+mongoose.connection.on("connected", () => {
+	console.log("connected to mongo yeah");
+});
 
-mongoose.connection.on('error',(err)=>{
-    console.log("err connecting", err)
-})
+mongoose.connection.on("error", (err) => {
+	console.log("err connecting", err);
+});
 
-require('./models/Trainee')
-require('./models/Trainer')
-require('./models/Training')
-require('./models/SportType')
+require("./models/Trainee");
+require("./models/Trainer");
+require("./models/Training");
+require("./models/SportType");
 
-const trainerAuth = require('./routes/TrainerAuth')
-const traineeAuth = require('./routes/TraineeAuth')
-const trainingAuth = require('./routes/TrainingAuth')
-const sportTypeAuth = require('./routes/sportTypeAuth')
+const trainerAuth = require("./routes/TrainerAuth");
+const traineeAuth = require("./routes/TraineeAuth");
+const trainingAuth = require("./routes/TrainingAuth");
+const sportTypeAuth = require("./routes/sportTypeAuth");
 
-app.use(express.json())
-app.use(trainerAuth, traineeAuth, trainingAuth, sportTypeAuth)
+app.use(express.json());
+app.use(trainerAuth);
+app.use(sportTypeAuth);
+app.use(trainingAuth);
+app.use(traineeAuth);
 
-app.listen(PORT,()=>{
-    console.log("server is running on", PORT)
-})
+app.listen(PORT, () => {
+	console.log("server is running on", PORT);
+});
--- routes/TraineeAuth.js
+++ routes/TraineeAuth.js
@@ -1,109 +1,138 @@
-const express = require('express')
-const Router = express.Router()
-const mongoose = require('mongoose')
-const Trainee = mongoose.model("Trainee")
-const Trainer = mongoose.model("Trainer")
-const Training = mongoose.model("Training")
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
-const {JWT_SECRET} = require('../key')
-const requireLogin = require('../middleware/requireLoginTrainee')
-const requireLoginTrainee = require('../middleware/requireLoginTrainee')
+const express = require("express");
+const Router = express.Router();
+const mongoose = require("mongoose");
+const Trainee = mongoose.model("Trainee");
+const Trainer = mongoose.model("Trainer");
+const Training = mongoose.model("Training");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../key");
+const requireLogin = require("../middleware/requireLoginTrainee");
+const requireLoginTrainee = require("../middleware/requireLoginTrainee");
 
+Router.post("/signupTrainee", (req, res) => {
+	const { username, email, password, age, tel } = req.body;
+	if (!email || !password || !username || !age || !tel) {
+		return res.status(422).json({ error: "please add all the fields" });
+	}
+	Trainee.findOne({ email: email })
+		.then((savedTrainee) => {
+			if (savedTrainee) {
+				return res
+					.status(422)
+					.json({ error: "Trainee already exist with that email" });
+			}
+			bcrypt.hash(password, 12).then((hashedpassword) => {
+				const trainee = new Trainee({
+					email,
+					password: hashedpassword,
+					age,
+					tel,
+					username,
+				});
+				trainee.save((err) => {
+					if (err) {
+						res.status(500).send({ message: err });
+						return;
+					}
+					res.json({ message: "saved successfully" });
+				});
+			});
+		})
 
-Router.post('/signupTrainee',(req,res)=>{
-    const {username,email,password,age,tel} = req.body
-    if(!email || !password || !username || !age || !tel ){
-        return res.status(422).json({error:"please add all the fields"})
-    }
-    Trainee.findOne({email:email})
-    .then((savedTrainee)=>{
-        if(savedTrainee){
-            return res.status(422).json({error:"Trainee already exist with that email"})
-        }
-        bcrypt.hash(password,12)
-        .then(hashedpassword=>{
-                const trainee = new Trainee({
-                    email,
-                    password:hashedpassword,
-                    age,
-                    tel,
-                    username
-                })
-                trainee.save(err => {
-                    if (err) {
-                      res.status(500).send({ message: err })
-                      return
-                    }
-                    res.json({message:"saved successfully"})
-                })
-        })
-        
-    })
+		.catch((err) => {
+			console.log(err);
+		});
+});
 
-    .catch(err=>{
-        console.log(err)
-    })
-})
+Router.post("/forgotPassword", (req, res) => {
+	const { email, password, tel } = req.body;
+	if (!email || !password || !tel) {
+		return res
+			.status(422)
+			.json({ error: "please add email or password or Tell" });
+	}
+	Trainee.findOne({ email, tel }).then((savedTrainee) => {
+		if (!savedTrainee) {
+			return res.status(422).json({ error: "Invalid email or tell" });
+		}
+		bcrypt.hash(password, 12).then((hashedpassword) => {
+			savedTrainee.password = hashedpassword;
+			savedTrainee.save((err) => {
+				if (err) {
+					res.status(500).send({ message: err });
+					return;
+				}
+				res.json({ message: "saved successfully" });
+			});
+		});
+	});
+});
 
+Router.post("/signinTrainee", (req, res) => {
+	const { email, password } = req.body;
+	if (!email || !password) {
+		return res.status(422).json({ error: "please add email or password" });
+	}
+	Trainee.findOne({ email: email }).then((savedTrainee) => {
+		if (!savedTrainee) {
+			return res.status(422).json({ error: "Invalid email or password" });
+		}
+		bcrypt
+			.compare(password, savedTrainee.password)
+			.then((doMatch) => {
+				if (doMatch) {
+					const token = jwt.sign({ _id: savedTrainee._id }, JWT_SECRET);
+					const { _id, username, email, tel, age, password } =
+						savedTrainee;
+					res.json({
+						token,
+						trainee: { _id, username, email, tel, age, password },
+					});
+				} else {
+					return res
+						.status(422)
+						.json({ error: "Invalid email or password" });
+				}
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	});
+});
 
-Router.post('/signinTrainee',(req,res)=>{
-    const {email,password} = req.body
-    if(!email || !password){
-        return res.status(422).json({error:"please add email or password"})
-    }
-    Trainee.findOne({email:email})
-    .then(savedTrainee=>{
-        if(!savedTrainee){
-           return res.status(422).json({error:"Invalid email or password"})
-        }
-        bcrypt.compare(password, savedTrainee.password)
-        .then(doMatch=>{
-            if(doMatch){
-                const token = jwt.sign({_id:savedTrainee._id},JWT_SECRET)
-                const {_id, username, email, tel, age, password} = savedTrainee
-                res.json({token, trainee:{ _id, username, email, tel, age, password}})
-             }
-            else{
-                return res.status(422).json({error:"Invalid email or password"})
-            }
-        })
-        .catch(err=>{
-            console.log(err)
-        })
-    })
-})
+Router.get("/TraineeDetails", (req, res) => {
+	Trainer.find({ _id: req.body })
+		.populate("-password")
+		.then((traineeDetails) => {
+			res.json({ traineeDetails });
+		})
+		.catch((err) => {
+			res.json(err);
+		});
+});
 
+Router.post("/editTraineeProfile", (req, res) => {
+	const { username, email, age, tel } = req.body;
+	if (!username || !email || !age || !tel) {
+		return res.status(422).json({ error: "please add all the fields" });
+	}
+	Trainer.findOne({ email: email }).then((savedTrainee) => {
+		if (!savedTrainee) {
+			return res.status(422).json({ error: "Trainee not found" });
+		}
+		savedTrainee.username = username;
+		savedTrainee.age = age;
+		savedTrainee.tel = tel;
+		savedTrainee
+			.save()
+			.then((trainee) => {
+				res.json({ message: "Updated trainee profile successfully" });
+			})
+			.catch((err) => {
+				res.json({ message: err });
+			});
+	});
+});
 
-Router.get("/TraineeDetails",(req,res)=>{
-    Trainer.find({_id: req.body})
-      .populate("-password")
-      .then((traineeDetails)=>{
-        res.json({traineeDetails})
-  })
-  .catch((err)=>{
-    res.json(err)
-  })
-  })
-  
-
-  Router.post('/editTraineeProfile',(req,res)=>{
-    const {username, email,age,tel} = req.body
-    if(!username ||!email || !age || !tel){
-        return res.status(422).json({error:"please add all the fields"})
-    }
-    Trainer.findOne({email:email})
-    .then(savedTrainee=>{
-        if(!savedTrainee){
-           return res.status(422).json({error:"Trainee not found"})
-        }
-        savedTrainee.username = username
-        savedTrainee.age = age
-        savedTrainee.tel = tel
-        savedTrainee.save()
-        .then((trainee)=> {res.json({message: "Updated trainee profile successfully"})})
-        .catch(err => {res.json({message: err})})
-    })
-})
-
-module.exports = Router
+module.exports = Router;
