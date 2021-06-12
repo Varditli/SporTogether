bb41cb2fb41a0e5a62dbba5e36aa1214ee2189bb
Varditli <vardit161@gmail.com>
2021-06-12 11:41:46 +0300

    up

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-//const PORT = process.env.PORT || 5000
-const PORT = 5000;
+const PORT = process.env.PORT || 5000
+//const PORT = 5000;
 const { MONGOURI } = require("./key");
 const cors = require("cors");
 app.use(cors());
